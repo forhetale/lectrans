a5689b683f56cb49358ecd59c37493a81bc9669e
forhetale <437752265@qq.com>
2026-05-08 23:11:35 +0900

    fix: Remove groq dependency from core module

--- core/__init__.py
+++ core/__init__.py
@@ -3,7 +3,6 @@ LecTrans 核心模块
 """
 
 from .audio_capture import AudioCapture, AudioChunk
-from .speech_recognizer import SpeechRecognizer, create_recognizer
 from .azure_speech_recognizer import AzureSpeechRecognizer, AzureSpeechRecognizerWithBuffer, create_azure_recognizer
 from .translator import Translator, Summarizer, create_translator, create_summarizer
 from .session_manager import SessionManager, SessionState, TranscriptEntry
@@ -11,8 +10,6 @@ from .session_manager import SessionManager, SessionState, TranscriptEntry
 __all__ = [
     "AudioCapture",
     "AudioChunk",
-    "SpeechRecognizer",
-    "create_recognizer",
     "AzureSpeechRecognizer",
     "AzureSpeechRecognizerWithBuffer",
     "create_azure_recognizer",
